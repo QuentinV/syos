cab017ddedd35d5a5768df29c7bea1c37817f7e8
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-06-09 02:49:35 +0200

    player picks cards draft

--- src/pages/Game/Running/Player/PPicksCards.tsx
+++ src/pages/Game/Running/Player/PPicksCards.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { usePlayerTurn, useStorytellerTurn } from '../../../../state/gameHooks';
+import { useUnit } from 'effector-react';
+import { $player } from '../../../../state/player';
+import { selectCard } from '../../../../state/game';
+import { GameCards } from '../../../../components/GameCards';
 
 export const PPicksCards: React.FC = () => {
-    return <></>;
+    const playerTurn = usePlayerTurn();
+    const [cardsVisible, setCardsVisible] = useState<boolean>(true);
+    const player = useUnit($player);
+    const storytellerTurn = useStorytellerTurn();
+
+    useEffect(() => {
+        if (playerTurn?.displayedCards?.length) {
+            setTimeout(() => {
+                setCardsVisible(false);
+            }, 2000);
+        }
+    }, [playerTurn?.displayedCards]);
+
+    if (!player || !playerTurn || !storytellerTurn) return null;
+
+    const onSelectCard = (index: number) => {
+        console.log('select card', index);
+        selectCard({ cardIndex: index, playerId: player!.id });
+    };
+
+    return (
+        <>
+            {!cardsVisible && <div>Story: {storytellerTurn.story}</div>}
+            {playerTurn.displayedCards?.length && (
+                <div>
+                    <div>
+                        Selected: {playerTurn?.selectedCards?.length ?? 0} / 3
+                    </div>
+                    <GameCards
+                        indexes={playerTurn.displayedCards}
+                        visible={cardsVisible}
+                        onSelect={onSelectCard}
+                        selected={playerTurn.selectedCards}
+                        limit={3}
+                    />
+                </div>
+            )}
+        </>
+    );
 };
